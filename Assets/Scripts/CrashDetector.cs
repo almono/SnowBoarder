c35b79af7e086a18dae27fe57952f592c909894b
almono <pwalczykiewicz93@gmail.com>
2023-12-03 15:46:47 +0100

    Crash tweak

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -8,10 +8,13 @@ public class CrashDetector : MonoBehaviour
     public ParticleSystem crashParticles;
     public AudioClip crashEffect;
 
+    private bool hasCrashed = false;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Ground"))
+        if (other.CompareTag("Ground") && !hasCrashed)
         {
+            hasCrashed = true;
             crashParticles.Play();
             GetComponent<AudioSource>().PlayOneShot(crashEffect);
             FindObjectOfType<PlayerController>().DisableControls();
